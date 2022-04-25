99cfc661597691fd42d86237840bb0d25b025527
Yuhan Chen <chenyh@umich.edu>
2022-04-24 19:06:28 -0700

    Set hlsearch to false by default

--- nvim/lua/basic/options.lua
+++ nvim/lua/basic/options.lua
@@ -6,7 +6,7 @@ local bo = vim.bo -- buffer-local optionlocal cmd = vim.cmd
 o.encoding = "utf-8"        -- set encoding
 o.updatetime = 100          -- delay to write swap to disk (default is 4000)
 o.timeoutlen = 500          -- timeout waiting for input
-o.showcmd = false           -- show command at the last line 
+o.showcmd = false           -- show command at the last line
 o.hidden = true             -- allow tab switch when buffer not saved
 o.termguicolors = true      -- enable xterm color compatibility
 o.cursorline = true         -- highlight current line
@@ -25,7 +25,7 @@ o.hlsearch = true           -- enable search highlight
 o.showmatch = true          -- breifly jump to matching paramthese
 o.wildmenu = true           -- enable cmd auto complete
 o.ignorecase = true         -- case insensitive when search all lowercase
-o.smartcase = true          -- case sensitive when search contains uppercase 
+o.smartcase = true          -- case sensitive when search contains uppercase
 o.spell = false             -- enable spell check
 o.spelllang = "en_us,cjk"   -- set spell check language to English
 o.foldenable = true         -- enable code fold
@@ -33,6 +33,7 @@ o.foldmethod = "indent"     -- set code fold strategy to indent
 o.foldlevel = 100           -- set max fold level
 o.splitbelow = true         -- splitbelow
 o.splitright = true         -- splitright
+o.hlsearch = false
 
 -- window-local options
 -- wo.number = false
