cb944a1ebb47e07ea42fe4cad3d341d731e1992c
Yuhan Chen <chenyh@umich.edu>
2022-04-24 19:16:02 -0700

    LSP working!

--- nvim/lua/conf/nvim-lsp-installer.lua
+++ nvim/lua/conf/nvim-lsp-installer.lua
@@ -70,6 +70,51 @@ else
     capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
 end
 
+local function lsp_highlight_document(client)
+  -- Set autocommands conditional on server_capabilities
+  if client.resolved_capabilities.document_highlight then
+    vim.api.nvim_exec(
+      [[
+      augroup lsp_document_highlight
+        autocmd! * <buffer>
+        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+      augroup END
+    ]],
+      false
+    )
+  end
+end
+
+local function lsp_keymaps(bufnr)
+  local opts = { noremap = true, silent = true }
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+  -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+  -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+  -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>f", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "[d", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', opts)
+  vim.api.nvim_buf_set_keymap(
+    bufnr,
+    "n",
+    "gl",
+    '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics({ border = "rounded" })<CR>',
+    opts
+  )
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+  vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
+end
+
+local on_attach = function(client, bufnr)
+  lsp_keymaps(bufnr)
+  lsp_highlight_document(client)
+end
+
 local servers = {
     'sumneko_lua',
     'pyright',
@@ -88,27 +133,13 @@ local servers = {
 for _, server_name in pairs(servers) do
     local server_available, server = lsp_installer_servers.get_server(server_name)
     if server_available then
-        -- -- Register a handler that will be called for all installed servers.
-        -- -- Alternatively, you may also register handlers on specific server instances instead (see example below).
-        -- lsp_installer.on_server_ready(function(server)
-        --     local opts = {
-        --         on_attach = handlers.on_attach,
-        --         capabilities = handlers.capabilities,
-        --     }
-
-
-        --     -- This setup() function is exactly the same as lspconfig's setup function.
-        --     -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-        --     server:setup(opts)
-        -- end)
-
         server:on_ready(function()
             -- When this particular server is ready (i.e. when installation is finished or the server is already installed),
             -- this function will be invoked. Make sure not to also use the "catch-all" lsp_installer.on_server_ready()
             -- function to set up your servers, because by doing so you'd be setting up the same server twice.
             local opts = {
-                -- on_attach = handlers.on_attach,
-                -- capabilities = handlers.capabilities,
+                on_attach = on_attach,
+                capabilities = capabilities,
             }
             if server_name == "jsonls" then
                 local jsonls_opts = require("lsp.settings.jsonls")
