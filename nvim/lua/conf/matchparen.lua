987351dd6402cf699dcb8dfc311324e54120160e
Yuhan Chen <chenyh@umich.edu>
2022-05-07 20:18:34 -0400

    Add matchparen plugin

---
+++ nvim/lua/conf/matchparen.lua
@@ -0,0 +1,13 @@
+local ok, matchparen = pcall(require, "matchparen")
+if not ok then
+    print("Warn: tried to load matchparen, but failed")
+else
+    matchparen.setup({
+        on_startup = true, -- Should it be enabled by default
+        hl_group = 'MatchParen', -- highlight group for matched characters
+        augroup_name = 'matchparen',  -- almost no reason to touch this unless there is already augroup with such name
+    })
+
+    vim.cmd("DoMatchParen")
+    vim.cmd("hi MatchParen cterm=none guibg=green guifg=orange")
+end
