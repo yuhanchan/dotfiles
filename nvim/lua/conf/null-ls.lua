285a14d112638aa0b128eff5c139806ace2bacd6
Yuhan Chen <chenyh@umich.edu>
2022-06-07 18:24:27 -0400

    Add autocommand to do formatting before saving

--- nvim/lua/conf/null-ls.lua
+++ nvim/lua/conf/null-ls.lua
@@ -2,6 +2,8 @@ local ok, null_ls = pcall(require, "null-ls")
 if not ok then
     print("Warn: tried to load null-ls, but failed")
 else
+    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
     null_ls.setup(
         {
         -- the binary executable of these tools must be installed to work
@@ -23,6 +25,22 @@ else
             -- null_ls.builtins.formatting.fixjson, -- json
             -- null_ls.builtins.formatting.gofmt, -- go
         },
+
+        -- setup autocommand for formatting before saving
+        on_attach = function(client, bufnr)
+            if client.supports_method("textDocument/formatting") then
+                vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+                vim.api.nvim_create_autocmd("BufWritePre", {
+                    group = augroup,
+                    buffer = bufnr,
+                    callback = function()
+                        -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+                        vim.lsp.buf.formatting_sync()
+                    end,
+                })
+            end
+        end,
+
     }
     )
 end
