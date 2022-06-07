eb00b02a6b000576547f8536b6b70f5e7c82ba0c
Yuhan Chen <chenyh@umich.edu>
2022-06-07 00:58:26 -0400

    Add mapping to prev_hunk and next_hunk

--- nvim/lua/conf/gitsigns.lua
+++ nvim/lua/conf/gitsigns.lua
@@ -83,6 +83,8 @@ else
                 -- map('n', '<leader>hu', gs.undo_stage_hunk)
                 -- map('n', '<leader>hR', gs.reset_buffer)
                 map('n', '<leader>gg', gs.preview_hunk)
+                map('n', '<leader>gp', gs.prev_hunk)
+                map('n', '<leader>gn', gs.next_hunk)
                 -- map('n', '<leader>hb', function() gs.blame_line{full=true} end)
                 -- map('n', '<leader>tb', gs.toggle_current_line_blame)
                 -- map('n', '<leader>hd', gs.diffthis)
