cece889b899701b043cdee4d4a0abb3a7c698913
Yuhan Chen <chenyh@umich.edu>
2022-06-09 01:39:17 -0400

    add keymaps for gitsigns functions

--- nvim/lua/conf/gitsigns.lua
+++ nvim/lua/conf/gitsigns.lua
@@ -78,7 +78,7 @@ else
 
                 -- Actions
                 -- map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
-                -- map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
+                map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
                 -- map('n', '<leader>hS', gs.stage_buffer)
                 -- map('n', '<leader>hu', gs.undo_stage_hunk)
                 -- map('n', '<leader>hR', gs.reset_buffer)
@@ -87,7 +87,7 @@ else
                 map('n', '<leader>gn', gs.next_hunk)
                 -- map('n', '<leader>hb', function() gs.blame_line{full=true} end)
                 -- map('n', '<leader>tb', gs.toggle_current_line_blame)
-                -- map('n', '<leader>hd', gs.diffthis)
+                map('n', '<leader>hd', gs.diffthis)
                 -- map('n', '<leader>hD', function() gs.diffthis('~') end)
                 -- map('n', '<leader>td', gs.toggle_deleted)
 
